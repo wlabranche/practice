841ad2ebe2b140439ddaaeae4a3f8d93a1e0853d
Will LaBranche <wlabranche@gmail.com>
2014-07-27 22:18:40 -0700

    added paths, minimla compile logic

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,26 @@ var htmlBuild = dist;
 
 var jsxFiles = 'app/jsx/**/*.jsx';
 
-// create paths
+// create paths here
+var vendorFiles = [
+    'bower_components/react/react-with-addons.js',
+    'node_modules/es6ify/node_modules/traceur/bin/traceur-runtime.js'];
+var vendorBuild = dist + '/vendor';
+var requireFiles = './node_modules/react/react.js';
 
+// general compile function
+var scriptCompile = function(watch){
+  gutil.log('browserification has begun...');
+
+  var entryFile = './app/jsx/app.jsx';
+  es6ify.tracurOverrides = {
+    experimental: true
+  };
+
+  var bundler = watch ? watchify(entryFile) : browserify(entryFile);
+
+
+};
 
 // this currently is broken/does nothing (will fix in future)
 gulp.task('vendor', function(){
@@ -32,3 +50,5 @@ gulp.task('html', function(){
   return gulp.src(htmlFiles)
     .pipe(gulp.dest(htmlBuild));
 });
+
+
